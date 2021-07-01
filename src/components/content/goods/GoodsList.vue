2bf621cf425605e379358bf2a78f2cea523bb87b
包谷清清 <776570787@qq.com>
2021-07-01 11:26:11 +0800

    home

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="(item,k) in goods" :goods-item="item" :key="k"/>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "@/components/content/goods/GoodsListItem";
+
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from 'vue-router'
+
+// 懒加载
+const Home = () => import('../views/home/Home.vue')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+
+// 1.安装插件
+Vue.use(VueRouter)
+
+// 2.创建路由
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: "首页",
+    component: Home
+  },
+  {
+    path: '/category',
+    name: '/category',
+    component:Category
+  },
+  {
+    path: '/cart',
+    name: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    name: '/profile',
+    component: Profile
+  }
+]
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+// 3.导出
+export default router
